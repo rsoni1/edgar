2a1906bb5f5dce7d9c41ff20b7c687ec4f3e17c2
Ritesh Soni <ritesh.soni@gmail.com>
2014-09-22 02:05:43 +0000

    Sample script to download index files from Edgar ftp site

---
+++ collect/getsample.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget ftp://ftp.sec.gov/edgar/full-index/2014/QTR1/company.idx
+wget ftp://ftp.sec.gov/edgar/full-index/2014/QTR1/master.idx
+wget ftp://ftp.sec.gov/edgar/full-index/2014/QTR1/form.idx
