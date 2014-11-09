2230c57c454001fb2197290e57cd042e7c1a9dbb
Ritesh Soni <ritesh.soni@gmail.com>
2014-11-09 23:25:41 +0000

    Added perl setup

--- collect/bootstrap-mrjob.sh
+++ collect/bootstrap-mrjob.sh
@@ -3,3 +3,4 @@ sudo yum install -y python-devel
 sudo yum install -y python-pip
 sudo pip install mrjob numpy nltk
 sudo python -m nltk.downloader -d /usr/share/nltk_data all
+sudo cpan install HTML::Parser
