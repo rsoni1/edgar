7f397def00cf233e75c3d77d539cead18e3d5121
Ritesh Soni <ritesh.soni@gmail.com>
2014-11-25 22:30:11 -0500

    adding bootstrap actions to install perl HTML::Parser module

--- collect/bootstrap-mrjob.sh
+++ collect/bootstrap-mrjob.sh
@@ -3,4 +3,6 @@ sudo yum install -y python-devel
 sudo yum install -y python-pip
 sudo pip install mrjob numpy nltk
 sudo python -m nltk.downloader -d /usr/share/nltk_data all
+yes | sudo yum install perl-CPAN
+yes | sed 's/y//' | sudo cpan install HTML::Parser
 sudo cpan install HTML::Parser
